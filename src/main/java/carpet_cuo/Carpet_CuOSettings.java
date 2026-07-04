2b73b4d09c343d3497f116e61da3b32c8a0a5db2
X-M-AwA <minecraft.xiaog@gmail.com>
2026-07-04 21:12:48 +0800

    Update Carpet_CuOSettings.java

--- src/main/java/carpet_cuo/Carpet_CuOSettings.java
+++ src/main/java/carpet_cuo/Carpet_CuOSettings.java
@@ -2,9 +2,6 @@ package carpet_cuo;
 
 import carpet.api.settings.Rule;
 import carpet.api.settings.Validators;
-import net.minecraft.world.level.ChunkPos;
-
-import java.util.ArrayList;
 
 import static carpet.api.settings.RuleCategory.*;
 
