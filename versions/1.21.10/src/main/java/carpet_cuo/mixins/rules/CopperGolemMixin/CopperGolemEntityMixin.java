c7f0a12a09aa44d2f2ef76289a847bdd453f8d32
X-M-AwA <minecraft.xiaog@gmail.com>
2026-08-13 22:55:34 +0800

    修复了copperGolemFix会导致游戏卡死的问题

--- versions/1.21.10/src/main/java/carpet_cuo/mixins/rules/CopperGolemMixin/CopperGolemEntityMixin.java
+++ versions/1.21.10/src/main/java/carpet_cuo/mixins/rules/CopperGolemMixin/CopperGolemEntityMixin.java
@@ -1,33 +1,29 @@
 package carpet_cuo.mixins.rules.CopperGolemMixin;
 
 import carpet_cuo.Carpet_CuOSettings;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.animal.coppergolem.CopperGolem;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.IOException;
-
 @Mixin(CopperGolem.class)
 public abstract class CopperGolemEntityMixin {
     @Inject(
             method = "turnToStatue",
-            at = @At("HEAD"),
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/level/ServerLevel;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"
+            ),
             cancellable = true
     )
-    private void copperGolem(CallbackInfo ci){
-        CopperGolem copperGolemEntity = (CopperGolem) (Object) this;
-        BlockPos blockPos = copperGolemEntity.blockPosition().below();
-        BlockState blockState;
-        try (Level world = copperGolemEntity.level()) {
-            blockState = world.getBlockState(blockPos);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if (Carpet_CuOSettings.copperGolemFix && blockState.isAir())ci.cancel();
+    private void copperGolem(CallbackInfo ci, @Local BlockPos blockPos, @Local(argsOnly = true) ServerLevel serverLevel){
+        BlockPos pos = blockPos.below();
+        BlockState blockState = serverLevel.getBlockState(pos);
+        if (Carpet_CuOSettings.copperGolemFix && blockState.isAir()) ci.cancel();
     }
 }
